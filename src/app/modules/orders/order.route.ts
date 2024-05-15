f0f8a60750eac193e3b49725efc8dc90081da642
Babul Akter <babulakterfsd@gmail.com>
2024-05-15 13:39:34 +0600

    order module added

--- src/app/modules/orders/order.route.ts
+++ src/app/modules/orders/order.route.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 
 //create order
 router.post(
-  '/',
+  '/init-payment',
   auth('customer'),
   validateRequest(orderValidationSchema),
   OrderControllers.initPayment,
