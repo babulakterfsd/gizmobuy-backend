7161033901306c10b892d485645868eb38dd8ad3
Babul Akter <babulakterfsd@gmail.com>
2024-06-23 14:22:24 +0600

    everything checked and deployed.

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['https://gizmobuy.vercel.app'],
+    origin: ['https://gizmobuy.vercel.app', 'http://localhost:5173'],
     credentials: true,
   }),
 );
